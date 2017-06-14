9913746ca716270a43732e91d661d1725c9cd5da
dbarraca <dbarraca@calpoly.edu>
2017-06-13 22:13:52 -0700

    No longer dummy description for post

--- public/Project/prjDetailController.js
+++ public/Project/prjDetailController.js
@@ -1,14 +1,15 @@
 app.controller('prjDetailController',
  ['$scope', '$state', '$stateParams', '$http', '$uibModal', 'notifyDlg', function($scope, $state, $stateParams, $http, $uibM, nDlg, ) {
    var prjId = $stateParams.prjId;
-
 //   $scope.skills = skls;
-   $scope.description = "dummyDescription in prjDetailController";
+   //$scope.description = "dummyDescription in prjDetailController";
 
    $http.get("/Prjs/" + prjId)
    .then(function(rsp) {
       console.log("rsp.data.title" + rsp.data.title);
-      $scope.prjTitle = rsp.data.title;
+      $scope.title = rsp.data.title;
+      $scope.type = rsp.data.type;
+      $scope.description = rsp.data.description;
    })
    .catch(function(err) {
       nDlg.show($scope, JSON.stringify(err));
